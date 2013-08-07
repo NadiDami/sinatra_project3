f90e387fe5a52a58b8194178928d85f8f96a537a
Nadia <nodunayo@gmail.com>
2013-08-07 22:08:48 +0100

    Created table page

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require 'sinatra'
+
+set :port, 8000
+set :bind, '127.0.0.1'
+
 require 'csv'
 
+
 def get_data
-    CSV.read('cfg_profiles.csv', :headers => true)
+    CSV.read('cfg_profiles.csv', :headers => true).to_a
 end
 
 get '/' do 
-    @data = data.to_a
+@data = get_data
+erb:table
 end
+
+get "/profiles/'<%=data[3]%>'" do
+erb:profile
+end 
